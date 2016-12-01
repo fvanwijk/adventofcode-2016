e54be5fac06b8533891a62cff16be9c129ba7a37
Frank van Wijk <fvanwijk@xebia.com>
2016-12-01 11:37:49 +0100

    Add solution for Day 1b

--- 1.js
+++ 1.js
@@ -1,30 +1,45 @@
 require('./helpers').getFile(1, input => {
+  function getDistance(coord) {
+    return Math.abs(coord.x) + Math.abs(coord.y);
+  }
+
   const data = input.split(', ');
   const result = data.reduce((pos, direction) => {
-    const [ match, bearing, steps, x, y ] = direction.match(/([LR])(\d+)/);
+    const [ match, bearing, steps ] = direction.match(/([LR])(\d+)/);
 
     // Determine new bearing
     pos.bearing = (pos.bearing + 360 + (bearing === 'R' ? 90 : -90)) % 360;
 
     // Walk
-    switch (pos.bearing) {
-      case 0:
-        pos.y += +steps;
-        break;
-      case 90:
-        pos.x += +steps;
-        break;
-      case 180:
-        pos.y -= +steps;
-        break;
-      case 270:
-        pos.x -= +steps;
-        break;
+    for (let i = 0; i < steps; i++) {
+      switch (pos.bearing) {
+        case 0:
+          pos.y++;
+          break;
+        case 90:
+          pos.x++;
+          break;
+        case 180:
+          pos.y--;
+          break;
+        case 270:
+          pos.x--;
+          break;
+      }
+
+      // Check if already visited this block
+      let newPos = `${pos.x}-${pos.y}`;
+      if (pos.blocks[newPos]) {
+        pos.visitedTwicePositions.push({ x: pos.x, y: pos.y });
+      }
+      pos.blocks[newPos] = true;
     }
 
     return pos;
-  }, { x: 0, y: 0, bearing: 0 });
+  }, { x: 0, y: 0, bearing: 0, blocks: {}, visitedTwicePositions: [] });
 
-  console.log('Blocks away from destination: ', Math.abs(result.x) + Math.abs(result.y));
+  console.log('Blocks away from destination: ', getDistance(result));
   // 301
+  console.log('Blocks away to first twice visited position: ', getDistance(result.visitedTwicePositions[0]));
+  // 130
 });
