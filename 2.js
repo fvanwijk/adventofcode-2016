6d32dac208e9bdb6113391626d0a27c916688a5d
Frank van Wijk <fvanwijk@xebia.com>
2016-12-02 10:32:21 +0100

    Add solution for Day 2b

--- 2.js
+++ 2.js
@@ -1,8 +1,17 @@
 require('./helpers').getFile(2, input => {
   const lines = input.split('\n').map(line => line.split(''));
-
+  const positions = [
+    [false, false, 1, false, false],
+    [false, 2, 3, 4, false],
+    [5, 6, 7, 8, 9],
+    [false, 'A', 'B', 'C', false],
+    [false, false, 'D', false, false]
+  ];
   let pos = [1, 1];
+  let pos2 = [2, 0];
   const code = [];
+  const code2 = [];
+
   lines.forEach(line => {
     line.forEach(move => {
       let posDelta;
@@ -26,10 +35,16 @@ require('./helpers').getFile(2, input => {
       if (newPos[0] >= 0 && newPos[0] < 3 && newPos[1] >= 0 && newPos[1] < 3) {
         pos = newPos;
       }
+      let newPos2 = [pos2[0] + posDelta[0], pos2[1] + posDelta[1]]; // newPos2 may be invalid
+      if (positions[newPos2[0]] && positions[newPos2[0]][newPos2[1]]) {
+        pos2 = newPos2;
+      }
     });
 
     code.push(pos[0] * 3 + pos[1] + 1);
+    code2.push(positions[pos2[0]][pos2[1]]);
   });
 
-  console.log('The bathroom code is:', code);
+  console.log('The first bathroom code is:', code);
+  console.log('The second bathroom code is:', code2);
 });
