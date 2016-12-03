4fbae760e20c0a90403d413697ac85a2d5843df2
Frank van Wijk <fvanwijk@xebia.com>
2016-12-03 12:43:57 +0100

    Add solution for Day 2b

--- 3.js
+++ 3.js
@@ -11,5 +11,19 @@ require('./helpers').getFile(3, input => {
     return checkTriangle([first, second, third]) ;
   });
 
+  const validColumnTriangles = input.replace(/\n/g, '').match(/(\d+)/g).reduce((sides, side, i) => {
+    sides.groupsOf3[i % 3].push(side);
+    // Check every 3 triangles and add to final list if it is a triangle
+    if (i % 9 == 8) {
+      let validGroups = sides.groupsOf3.filter(checkTriangle);
+      sides.triangles = sides.triangles.concat(validGroups);
+      sides.groupsOf3 = [[], [], []];
+    }
+    return sides;
+  }, { triangles: [], groupsOf3: [[], [], []]}).triangles;
+
   console.log('Number of valid triangles:', validTriangles.length);
+  // 993
+  console.log('Number of valid triangles in columns:', validColumnTriangles.length);
+  // 1849
 });
