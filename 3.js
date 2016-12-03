ddb60ff5f3f8970dd132d7e7af1a6d84d104bc98
Frank van Wijk <fvanwijk@xebia.com>
2016-12-03 12:01:15 +0100

    Use helper function

--- 3.js
+++ 3.js
@@ -1,9 +1,14 @@
+function checkTriangle(t) {
+  const a = +t[0], b = +t[1], c = +t[2];
+  return a + b > c
+  && a + c > b
+  && b + c > a
+}
+
 require('./helpers').getFile(3, input => {
   const validTriangles = input.split('\n').filter(line => {
     const [match, first, second, third] = line.match(/(\d+)\s+(\d+)\s+(\d+)/);
-    return +first + +second > +third
-           && +first + +third > +second
-           && +second + +third > +first;
+    return checkTriangle([first, second, third]) ;
   });
 
   console.log('Number of valid triangles:', validTriangles.length);
