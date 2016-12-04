739e813b760e4beeb9774f855cfb222efb5b2114
Frank van Wijk <fvanwijk@xebia.com>
2016-12-04 16:20:55 +0100

    Add solution for Day 4b

--- 4.js
+++ 4.js
@@ -1,5 +1,5 @@
 require('./helpers').getFile(4, input => {
-  const realRoomsSectorIdTotal = input.split('\n').reduce((acc, line) => {
+  const realRooms = input.split('\n').reduce((acc, line) => {
     const tokens = line.split('-');
     const [match, id, checksum] = tokens.splice(-1).join('').match(/(\d+)\[(\w+)\]/);
 
@@ -24,12 +24,32 @@ require('./helpers').getFile(4, input => {
     });
     const countList = sortedCounts.slice(0, 5).map(count => count.letter).join('');
     if (checksum === countList) {
-      acc += +id;
+      acc.push({
+        id: +id,
+        checksum,
+        tokens
+      });
     }
 
     return acc;
-  }, 0);
+  }, []);
 
-  console.log('Room rooms sector ID sum:', realRoomsSectorIdTotal);
+  // Ceasar decipher room names
+  const decodedNorthPoleObjectStorageRoom = realRooms.filter(room => {
+    return room.tokens.join('-').split('').map(token => {
+      if (token === '-') {
+        token = ' ';
+      } else {
+        for(let i = 0; i < room.id; i++) {
+          token = token === 'z' ? 'a' : String.fromCharCode(token.charCodeAt(0) + 1);
+        }
+      }
+      return token;
+    }).join('') === 'northpole object storage';
+  });
+
+  console.log('Room rooms sector ID sum:', realRooms.reduce((total, room) => total + room.id, 0));
   // 361724
+  console.log('Room with the Northpole Object Storage:', decodedNorthPoleObjectStorageRoom[0].id);
+  // 482
 });
