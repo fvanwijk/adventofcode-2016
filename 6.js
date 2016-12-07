de93aaa75ee0b58d29e70673d5b3c7010dc68c46
Frank van Wijk <fvanwijk@xebia.com>
2016-12-07 09:55:05 +0100

    Add solution to Day 6b

--- 6.js
+++ 6.js
@@ -8,15 +8,22 @@ require('./helpers').getFile(6, input => {
 
   const countsArray = counts.reduce((word, letterCounts) => {
     let countWinner = [0];
+    let countLoser = [99999];
     for (let letter in letterCounts) {
       if (countWinner[0] < letterCounts[letter]) {
         countWinner = [letterCounts[letter], letter];
       }
+      if (countLoser[0] > letterCounts[letter]) {
+        countLoser = [letterCounts[letter], letter];
+      }
     }
-    word.push(countWinner[1]);
+    word.winner.push(countWinner[1]);
+    word.loser.push(countLoser[1]);
     return word;
-  }, []);
+  }, {loser:[],winner:[]});
 
-  console.log('The message is:', countsArray.join(''));
+  console.log('The message is:', countsArray.winner.join(''));
   // zcreqgiv
+  console.log('The second message is:', countsArray.loser.join(''));
+  // pljvorrk
 });
