018b1bae27db06b6d98f34a76dd1b6cad54a86c1
Frank van Wijk <fvanwijk@xebia.com>
2016-12-10 15:39:58 +0100

    Even more ES6 and bitwise optimalisations

--- 7.js
+++ 7.js
@@ -1,39 +1,26 @@
 require('./helpers').getFile(7, input => {
+  const abaReg = /(\w)(?=([^\1]\1))/g;
   const result = input.split('\n').reduce((acc, line, i) => {
-    var matches = line.match(/([^\[\]$]+)+/g);
 
     // Create supernet and hypernet lists with triples
-    const groupMatches = matches.reduce((acc2, wordGroup, i) => {
+    const groupMatches = line.match(/([^\[\]$]+)+/g).reduce((acc2, wordGroup, i) => {
 
       // Validates ABBA match (intermediate result)
-      const hasAbba = wordGroup.search(/([a-z])((?!\1).)\2\1/g) != -1;
-      acc2.abba[i % 2] = i % 2 ? (acc2.abba[i % 2] && !hasAbba) : (acc2.abba[i % 2] || hasAbba);
+      acc2.abba[i % 2] |= /(\w)((?!\1)\w)\2\1/.test(wordGroup);
 
       // Matches ABA in supernet or hypernet and adds to the respective list
-      const abaMatches = [];
       let val;
-      const abaReg = /(\w)(?=([^\1]\1))/g;
       while (val = abaReg.exec(wordGroup)) {
-        abaMatches.push(val[1] + val[2]);
+        acc2.aba[i % 2].push(val[1] + val[2]);
       }
-      if (abaMatches) {
-        acc2.aba[i % 2] = acc2.aba[i % 2].concat(abaMatches);
-      }
-
       return acc2;
-    }, { abba: [false, true], aba: [[],[]] });
+    }, { abba: [0, 0], aba: [[],[]] });
 
     // Aggregate line: validation on ABBA rules
-    if (groupMatches.abba[0] && groupMatches.abba[1]) {
-      acc.abba++;
-    }
+    acc.abba += groupMatches.abba[0] & ~groupMatches.abba[1];
 
     // Aggregate line: validation on ABA rules
-    if (groupMatches.aba[0].some(word => {
-        return groupMatches.aba[1].indexOf(word[1] + word[0] + word[1]) !== -1;
-      })) {
-      acc.aba++;
-    }
+    acc.aba += groupMatches.aba[0].some(word => ~groupMatches.aba[1].indexOf(word[1] + word[0] + word[1]));
     return acc;
   }, { abba: 0, aba: 0 });
 
