2c78d813cf286b9c325e714f2def3f47dd132b67
Frank van Wijk <fvanwijk@xebia.com>
2016-12-10 14:55:08 +0100

    Add solution to Day 7a+b

---
+++ 7.js
@@ -0,0 +1,44 @@
+require('./helpers').getFile(7, input => {
+  const result = input.split('\n').reduce((acc, line, i) => {
+    var matches = line.match(/([^\[\]$]+)+/g);
+
+    // Create supernet and hypernet lists with triples
+    const groupMatches = matches.reduce((acc2, wordGroup, i) => {
+
+      // Validates ABBA match (intermediate result)
+      const hasAbba = wordGroup.match(/([a-z])((?!\1).)\2\1/g);
+      acc2.abba[i % 2] = i % 2 ? (acc2.abba[i % 2] && (hasAbba == null)) : (acc2.abba[i % 2] || (hasAbba != null));
+
+      // Matches ABA in supernet or hypernet and adds to the respective list
+      const abaMatches = [];
+      let val;
+      const abaReg = /(\w)(?=([^\1]\1))/g;
+      while (val = abaReg.exec(wordGroup)) {
+        abaMatches.push(val[1] + val[2]);
+      }
+      if (abaMatches) {
+        acc2.aba[i % 2] = acc2.aba[i % 2].concat(abaMatches);
+      }
+
+      return acc2;
+    }, { abba: [false, true], aba: [[],[]] });
+
+    // Aggregate line: validation on ABBA rules
+    if (groupMatches.abba[0] && groupMatches.abba[1]) {
+      acc.abba++;
+    }
+
+    // Aggregate line: validation on ABA rules
+    if (groupMatches.aba[0].some(word => {
+        return groupMatches.aba[1].indexOf(word[1] + word[0] + word[1]) !== -1;
+      })) {
+      acc.aba++;
+    }
+    return acc;
+  }, { abba: 0, aba: 0 });
+
+  console.log('Number of TLS email adresses:', result.abba);
+  // 118
+  console.log('Number of SSL email adresses:', result.aba);
+  // 260
+});
