c73da40f8e9eb13f96d12b4ee5524879ab6b3588
Frank van Wijk <fvanwijk@xebia.com>
2016-12-11 10:20:43 +0100

    Add solution to Day 8a

---
+++ 8.js
@@ -0,0 +1,46 @@
+require('./helpers').getFile(8, input => {
+  function arrayRotate(arr, count) {
+    count -= arr.length * Math.floor(count / arr.length);
+    arr.push.apply(arr, arr.splice(0, count));
+    return arr;
+  }
+
+  const height = 6;
+  const width = 50;
+  const initArray = [];
+  for (let i = 0; i < height; i++) {
+    initArray[i] = new Array(width).fill(false);
+  }
+
+  const keypad = input.split('\n').reduce((acc, line) => {
+    const command = line.split(' ');
+    if (command[0] == 'rect') {
+      // Fill rect from x width and y tall with 'true'
+      const [match, x, y] = command[1].match(/(\d+)x(\d+)/);
+      for (let i = 0; i < +x; i++) {
+        for (let j = 0; j < +y; j++) {
+          acc[j][i] = true;
+        }
+      }
+    } else {
+      let [rotate, direction, position, by, steps] = command;
+      const [axis, positionIndex] = position.match(/\w=(\d+)/);
+      if (direction == 'row') {
+        acc[positionIndex] = arrayRotate(acc[positionIndex], -steps);
+      } else {
+        const col = acc.map(row => row[positionIndex]);
+        const rotatedCol = arrayRotate(col, -steps);
+        acc.forEach((row, i) => {
+          row[positionIndex] = col[i];
+        });
+      }
+    }
+
+    return acc;
+  }, initArray);
+
+  const count = keypad.reduce((count, row) => row.reduce((x, y) => x + y, count), 0);
+
+  console.log('The code is:', count);
+  // 128
+});
