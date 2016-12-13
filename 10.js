3310dd626327c0cf0f6be1e77c82dc7592e16c33
Frank van Wijk <fvanwijk@xebia.com>
2016-12-13 08:28:12 +0100

    Clean up code for Day 10

--- 10.js
+++ 10.js
@@ -1,83 +1,63 @@
 require('./helpers').getFile(10, input => {
-  const agents = {};
-
-//   input = `value 5 goes to bot 2
-// bot 2 gives low to bot 1 and high to bot 0
-// value 3 goes to bot 1
-// bot 1 gives low to output 1 and high to bot 0
-// bot 0 gives low to output 2 and high to output 0
-// value 2 goes to bot 2`;
-
-  function init(id, type) {
-    const i = `${id}-${type}`;
-    if (!agents[i]) { agents[i] = { id, type, rules: [], values: [], matches: [] }; }
+  function get(list, id) {
+    return list.filter(n => n.id == id)[0];
+  }
+  function getValues(bot) {
+    return get(bot.type == 'bot' ? bots : outputs, bot.id).values;
+  }
+  function outputVal(i) {
+    return get(outputs, i).values[0];
   }
 
+  // Init
+  function init(t) {
+    return n => ({
+      id: n.slice(t),
+      values: []
+    });
+  }
+  const bots = input.match(/bot \d+/g).map(init(4));
+  const outputs = input.match(/output \d+/g).map(init(7));
+  let botWith61And17;
+
   input.split('\n').map(line => {
-    // Get instruction info
+    // Save all instructions
     if (line[0] == 'v') {
       [match, value, botId] = line.match(/value (\d+) goes to bot (\d+)/);
-      init(botId, 'bot');
-      agents[`${botId}-bot`].values.push(+value);
+      get(bots, botId).values.push(+value);
     } else {
       [match, botId, lowSubject, lowId, highSubject, highId] = line.match(/bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)/);
-      init(botId, 'bot');
-      init(lowId, lowSubject);
-      init(highId, highSubject);
-      if (!agents[`${botId}-bot`]) {
-        console.log(`Bot ${botId} never got a value. Unable to give low to ${lowSubject} ${lowId} or high to ${highSubject} ${highId}`);
-      } else {
-        agents[`${botId}-bot`].rules.push({ low: `${lowId}-${lowSubject}`, high: `${highId}-${highSubject}`})
-      }
+      get(bots, botId).low = { id: lowId, type: lowSubject };
+      get(bots, botId).high = { id: highId, type: highSubject };
     }
   });
 
   function process() {
-    let actionCount = 0;
-    for (agentId in agents) {
+    isStable = true;
 
-      const agent = agents[agentId];
-      if (agent.values.length > 1 && agent.type == 'bot') {
-        const lowAgent = agents[agent.rules[0].low];
-        //console.log(`Passing ${Math.min.apply(this, agent.values)} from ${agent.type} ${agent.id} to ${lowAgent.type} ${lowAgent.id}`);
-        lowAgent.values.push(Math.min.apply(this, agent.values));
+    bots.forEach(bot => {
+      if (bot.values.length > 1) {
+        getValues(bot.low).push(Math.min.apply(this, bot.values));
+        getValues(bot.high).push(Math.max.apply(this, bot.values));
 
-        const highAgent = agents[agent.rules[0].high];
-        //console.log(`Passing ${Math.max.apply(this, agent.values)} from ${agent.type} ${agent.id} to ${highAgent.type} ${highAgent.id}`);
-        highAgent.values.push(Math.max.apply(this, agent.values));
+        if (bot.values.some(x => x == 61) && bot.values.some(x => x == 17)) {
+          botWith61And17 = bot;
+        }
 
-        agent.matches.push(agent.values);
-        agent.values = [];
-        actionCount++;
+        bot.values = [];
+        isStable = false;
       }
-    }
-
-    if (actionCount) {
-      process();
-    }
+    });
   }
 
-  process();
-
-  function containsMatch(agent, first, second) {
-    return agent.matches.filter(match => {
-      return (match[0] == first && match[1] == second) || (match[1] == first && match[0] == second);
-    }).length > 0;
+  let isStable = false;
+  while (!isStable) {
+    process();
   }
 
-  let found = false;
-  for (id in agents) {
-    found = containsMatch(agents[id], 61, 17);
-    if (found) {
-      console.log('Bot that passes 61 and 17: ', agents[id].id);
-      // 113
-      break;
-    }
-  }
+  console.log('Bot that passes 61 and 17: ', botWith61And17.id);
+  // 113
 
-  function outputVal(i) {
-    return agents[`${i}-output`].values[0];
-  }
   console.log('First three outputs multiplied: ', outputVal(0) * outputVal(1) * outputVal(2));
   // 12803
 });
